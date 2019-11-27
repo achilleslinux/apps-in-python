500f6c024cafda16425dc473c4770dee277a47b0
Avinash Kumar <achilleslinux@gmail.com>
2019-11-27 20:17:43 +0530

    added youtube video downloader script

---
+++ youtube_downloader/download.py
@@ -0,0 +1,29 @@
+
+import pytube
+#from pytube import YouTube 
+  
+#where to save 
+#SAVE_PATH = "E:/" #to_do 
+  
+#link of the video to be downloaded 
+link="https://www.youtube.com/watch?v=VZk9mdVLb5w"
+  
+try: 
+    #object creation using YouTube which was imported in the beginning 
+    yt = pytube.YouTube(link) 
+except: 
+    print("Connection Error") #to handle exception 
+  
+#filters out all the files with "mp4" extension 
+mp4files = yt.filter('mp4') 
+  
+yt.set_filename('GeeksforGeeks Video') #to set the name of the file 
+  
+#get the video with the extension and resolution passed in the get() function 
+d_video = yt.get(mp4files[-1].extension,mp4files[-1].resolution) 
+try: 
+    #downloading the video 
+    d_video.download() 
+except: 
+    print("Some Error!") 
+print('Task Completed!') 
