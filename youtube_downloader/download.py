192480aaf42212fc461b03660978d98c00ba0b17
Avinash Kumar <achilleslinux@gmail.com>
2019-12-02 12:40:59 +0530

    added youtube download filw

--- youtube_downloader/download.py
+++ youtube_downloader/download.py
@@ -1,29 +1,35 @@
 
-import pytube
-#from pytube import YouTube 
+# from pytube import YouTube
+# #from pytube import YouTube 
   
-#where to save 
-#SAVE_PATH = "E:/" #to_do 
+# #where to save 
+# #SAVE_PATH = "E:/" #to_do 
   
-#link of the video to be downloaded 
-link="https://www.youtube.com/watch?v=VZk9mdVLb5w"
+# #link of the video to be downloaded 
+# link="https://www.youtube.com/watch?v=VZk9mdVLb5w"
   
-try: 
-    #object creation using YouTube which was imported in the beginning 
-    yt = pytube.YouTube(link) 
-except: 
-    print("Connection Error") #to handle exception 
-  
-#filters out all the files with "mp4" extension 
-mp4files = yt.filter('mp4') 
-  
-yt.set_filename('GeeksforGeeks Video') #to set the name of the file 
-  
-#get the video with the extension and resolution passed in the get() function 
-d_video = yt.get(mp4files[-1].extension,mp4files[-1].resolution) 
-try: 
-    #downloading the video 
-    d_video.download() 
-except: 
-    print("Some Error!") 
-print('Task Completed!') 
+# yt = YouTube(link) 
+ 
+# yt = yt.get('mp4','720p')
+# yt.download('.')
+# print('Task Completed!')
+
+from pytube import YouTube
+import os,tqdm
+
+def downloadYouTube(videourl, path):
+
+    yt = YouTube(videourl)
+    yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    if not os.path.exists(path):
+        os.makedirs(path)
+    yt.download(path)
+    with tqdm(total=file_size, unit='B',
+              unit_scale=True, unit_divisor=1024,
+              desc='government_interest.tsv.zip', initial=initial_pos,
+              ascii=True, miniters=1) as pbar:
+        for chunk in r.iter_content(32 * 1024):
+            f.write(chunk)
+            pbar.update(len(chunk))
+
+downloadYouTube('https://www.youtube.com/watch?v=VZk9mdVLb5w', './videos/FindingNemo1')
